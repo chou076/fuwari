7779260e9cc3d334f67f9751ab8f8c4105562b39
chou076 <91853707+chou076@users.noreply.github.com>
2025-12-23 22:45:32 +0800

    Fix typo in profileConfig bio

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Kaki",
-	bio: "这个人很懒什么都没有留下...",
+	bio: "这个人很懒什么都没有留下..",
 	links: [
 		{
 			name: "Twitter",
